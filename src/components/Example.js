9a83f3ef1635ff8807aa351aa6b284674cb87ed8
Polina Chernomaz <polina@crosscompute.com>
2019-10-13 19:06:12 -0400

    example html rendered using material-ui typography
    - getMarkdownText():
        - currently returns an array of objects with key: tag (HTML tag), and value:
        values (an array of string(s)). The tag and text are separated so that in render, the
        Typography JSX can assign variant to the tag, and the text be
        separate.
        - added data in data/example.js to const input for easier view while fixing
        the markdown rendering.
        - used .map() method to pass array elements to marked method from markedjs
        (which is a markdown parser and compiler; converts to HTML)
    
    - render()
        - const markd calls getMarkDownText() to get the results.
        - variantMapping takes care of possible <li>, for now, that
        could result from markdown->html. We change the mapping of body1 to 'li',
        and after rendering the markdown using Typography, surround 'li' with
        <ul> tags. Otherwise, render the markdown as usual.
        - return()
            - adding {markTags} to evaluate the variable markTags which
            renders the markdown using material-ui Typography.
            - tested ReactMarkdown on the file data/example.js to see how it renders
            - added Typography h1 to keep a reference of what h1 should look
            like.
    
    Next steps: separate HTML tags and text of the result of marked(ma) in
    getMarkDownText() and return as an array of objects with key: tag,
    values: text.

--- src/components/Example.js
+++ src/components/Example.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
+import ReactMarkdown from 'react-markdown';
 import marked from 'marked';
-import 'typeface-roboto';
 import Typography from '@material-ui/core/Typography';
 import ExampleData from '../data/example'
 
@@ -8,27 +8,77 @@ class Example extends Component {
   constructor() {
     super();
     this.state = {
-      arr: []
+      arr: [],
+      dict: {},
     }
   }
-
+  
   componentDidMount() {
     this.setState({arr: ExampleData})
   }
-
+  /**
+   * this function should return an obj
+   * the keys should be the html tag
+   * the values should be a list of the markdown that corressponds to those tags
+   * 
+   * input = [
+        "# Locate Fire Stations",
+        "- one",
+        "- two",
+    ]
+   * 1. pass the arr elements to marked method
+   * 2. parse HTML to separate tags and text to key and values
+   * 
+   */
   getMarkdownText() {
-    var arrayLength = this.state.arr.length
-    var rawMakeup = ""
-    for (var i = 0; i<arrayLength; i++){
-      rawMakeup += marked(this.state.arr[i])
-    }
-    return {__html: rawMakeup}
+    const input = [
+      "# Locate Fire Stations",
+      "- one",
+      "- two",
+    ]
+    const markedArr = input.map(function(ma){
+      return marked(ma);
+    });
+    return [{
+      tag: 'h1',
+      values: ['Hello World']
+    },
+    {
+      tag: 'li',
+      values: ['one', 'two']
+    }]
   }
   
   render() {
+    /**
+     * 1. call function and get result
+     * 2. assign key from result to typography variant
+     * 
+     */
+    const markd = this.getMarkdownText();
+    const variantMapping = { h1: 'h1', h2: 'h2', h3: 'h3', h4: 'h4', h5: 'h5', h6: 'h6', subtitle1: 'h6', subtitle2: 'h6', body1: 'li', body2: 'p',}
+    const markTags = markd.map(function (m) {
+      let variant = null;
+      if (m.tag == 'h1'){
+        variant = 'h1'
+      }else if(m.tag == 'li'){
+        variant = 'body1'
+      }
+      const tag = m.values.map(function (v, i){
+        return <Typography key={i} variant={variant} variantMapping={variantMapping}>{v}</Typography>;
+      });
+      if (m.tag == 'li') {
+        // what is tag - list of typography jsx
+        return <ul>{tag}</ul>
+      } else {
+        return tag;
+      }
+    });
     return (
       <div>
-        <Typography dangerouslySetInnerHTML={this.getMarkdownText()}></Typography>
+        {markTags}
+        <ReactMarkdown source={this.state.arr.join('\n')}></ReactMarkdown>
+        <Typography variant="h1">h1. Heading</Typography> 
       </div>
     );
   }
